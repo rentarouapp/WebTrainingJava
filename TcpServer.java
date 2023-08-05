416be4dce454a19f8c0d1d762c55965a3840df12
uejo <rentarou1994@gmail.com>
2023-08-05 23:47:43 +0900

    :art: ServerとClientで双方向通信ができるまで

--- TcpServer.java
+++ TcpServer.java
@@ -7,14 +7,14 @@ public class TcpServer {
     public static void main(String[] args) throws Exception {
         try (ServerSocket server = new ServerSocket(8001);
                 FileOutputStream fos = new FileOutputStream("server_recv.txt");
-                FileInputStream fis = new FileInputStream("server_send.text")) {
+                FileInputStream fis = new FileInputStream("server_send.txt")) {
 
             System.out.println("クライアントからの接続を待ちます");
             Socket socket = server.accept();
             System.out.println("クライアント接続");
 
             int ch;
-            // クライアントから受け取った内容をserver_rect.txtに出力
+            // クライアントから受け取った内容をserver_recv.txtに出力
             InputStream input = socket.getInputStream();
             // クライアントは終了のマークとして0を送付してくる
             while ((ch = input.read()) != 0) {
@@ -22,8 +22,8 @@ public class TcpServer {
             }
             // server_send.txtの内容をクライアントに送付
             OutputStream output = socket.getOutputStream();
-            while ((ch = fis.read() != -1)) {
-                fos.write(ch);
+            while ((ch = fis.read()) != -1) {
+                output.write(ch);
             }
             socket.close();
             System.out.println("通信を終了しました");
