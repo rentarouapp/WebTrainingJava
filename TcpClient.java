5564d1a938cfa6c474c6f2788ee24e5a4fdf3fe8
uejo <rentarou1994@gmail.com>
2023-08-06 14:50:24 +0900

    :art: javaの手製クライアントからapacheを叩けるようにする

--- TcpClient.java
+++ TcpClient.java
@@ -3,7 +3,7 @@ import java.net.*;
 
 public class TcpClient {
     public static void main(String[] args) throws Exception {
-        try (Socket socket = new Socket("localhost", 8001);
+        try (Socket socket = new Socket("localhost", 80);
             FileInputStream fis = new FileInputStream("client_send.txt");
             FileOutputStream fos = new FileOutputStream("client_recv.txt")) {
                 
@@ -14,7 +14,7 @@ public class TcpClient {
                     output.write(ch);
                 }
                 // 終了を示すために0を送信
-                output.write(0);
+                //output.write(0);
                 // サーバからの返信をclient_recv.txtに出力
                 InputStream input = socket.getInputStream();
                 while ((ch = input.read()) != -1) {
